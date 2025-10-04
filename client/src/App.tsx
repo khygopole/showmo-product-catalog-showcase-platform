b1b0090e7a559348e3a678006b79bf5eac1fb2eb
Khyle Matthew Gopole <khylematthewgopole@gmail.com>
2025-10-04 18:27:51 +0800

    Initial Folder Setup

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500">INITIAL SETUP</h1>
+      <h2 className="text-green-500 font-bold">TAILWINDCSS AND ROUTER SETUP</h2>
+    </>
+  );
+}
+
+export default App;

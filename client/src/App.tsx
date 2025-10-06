97cf2eb83f544b07cf0db287346806197aea24bf
Khyle Matthew Gopole <khylematthewgopole@gmail.com>
2025-10-06 21:01:30 +0800

    Added UI for Landing Page, Login, and Register in Client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
 
 function App() {
   return (
-    <>
-      <h1 className="text-red-500">INITIAL SETUP</h1>
-      <h2 className="text-green-500 font-bold">TAILWINDCSS AND ROUTER SETUP</h2>
-    </>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   );
 }
 
